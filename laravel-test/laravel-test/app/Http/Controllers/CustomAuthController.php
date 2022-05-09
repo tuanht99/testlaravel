cfc6eb14d3231e48e48d0629ade6fa066b7f81f2
Hang Thanh Tuan <tuanht.02.student@fit.tdc.edu.vn>
2022-05-09 22:03:48 +0700

    a

---
+++ laravel-test/laravel-test/app/Http/Controllers/CustomAuthController.php
@@ -0,0 +1,89 @@
+<?php
+namespace App\Http\Controllers;
+use Illuminate\Http\Request;
+use Hash;
+use Session;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+
+//Unknow
+class CustomAuthController extends Controller
+{
+    public function index()
+    {
+        return view('auth.login');
+    }
+
+    public function customLogin(Request $request)
+    {
+        $input = $request->all();
+        $request->validate([
+            'username' => 'required',
+            'password' => 'required',
+        ]);
+
+        $fieldType = filter_var($request->username, FILTER_VALIDATE_EMAIL) ? 'email' : 'name';
+        if(auth()->attempt(array($fieldType => $input['username'], 'password' => $input['password'])))
+        {
+            return view('dashboard');
+        }else{
+            return redirect()->route('login')
+                ->with('error','Email-Address And Password Are Wrong.');
+        }
+        // $credentials = $request->only('email', 'password');
+        // if (Auth::attempt($credentials)) {
+        //     return redirect()->intended('dashboard')
+        //         ->withSuccess('Signed in');
+        // }
+
+        // return redirect("login")->withSuccess('Login details are not valid');
+    }
+
+    public function registration()
+    {
+        return view('auth.registration');
+    }
+
+    public function customRegistration(Request $request)
+    {
+        $request->validate([
+            'name' => 'required',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|min:6'
+        ]);
+
+        $data = $request->all();
+        var_dump($data);
+        $check = $this->create($data);
+
+        return redirect("dashboard")->withSuccess('You have signed-in');
+    }
+
+    public function create(array $data)
+    {
+        return User::create([
+            'name' => $data['name'],
+            'email' => $data['email'],
+            'password' => Hash::make($data['password']),
+            'first_name' => $data['firstName'],
+            'last_name' => $data['lastName'],
+            'phone' => $data['phone']
+        ]);
+    }
+
+    public function dashboard()
+    {
+        if(Auth::check()){
+            return view('dashboard');
+        }
+
+        return redirect("login")->withSuccess('You are not allowed to access');
+    }
+
+    public function signOut() {
+        Session::flush();
+        Auth::logout();
+
+        return Redirect('login');
+    }
+}
